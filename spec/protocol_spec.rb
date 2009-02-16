8214595343b035c446e5f6a41f2861b9c543d1e4
Bernhard Stoeckner <elven@swordcoast.net>
2009-02-16 11:06:57 +0100

    Protocol: update spec to not block on reads with not enough data

--- spec/protocol_spec.rb
+++ spec/protocol_spec.rb
@@ -1,11 +1,15 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe "ProtocolChain", :shared => true do
+describe "ProtocolChainSetup", :shared => true do
   it_should_behave_like "IO Mockup"
 
   before do
     @chain = Arpie::ProtocolChain.new(*subject)
   end
+end
+
+describe "ProtocolChain", :shared => true do
+  it_should_behave_like "ProtocolChainSetup"
 
   it "should convert without io correctly" do
     v = @chain.to "x"
@@ -13,19 +17,21 @@ describe "ProtocolChain", :shared => true do
   end
 
   it "should read written binary data correctly" do
-    @chain.write_message(@w, "x")
-    @chain.read_message(@r).should == "x"
+    chain_write("x")
+    chain_read.should == "x"
     @chain.buffer.size.should == 0
     @chain.messages.size.should == 0
   end
 
   it "should read multiple written messages correctly" do
+    write = []
     for i in 1...10 do
-      @chain.write_message(@w, "x#{i}")
+      write << "x#{i}"
     end
+    chain_write(*write)
 
     for i in 1...10 do
-      @chain.read_message(@r).should == "x#{i}"
+      chain_read.should == "x#{i}"
     end
 
     @chain.buffer.size.should == 0
@@ -33,28 +39,32 @@ describe "ProtocolChain", :shared => true do
   end
 
   it "should not clobber the buffer before the first read" do
-    @chain.write_message(@w, "hi")
+    chain_write("hi")
     @chain.buffer.size.should == 0
     @chain.messages.size.should == 0
   end
 
   it "should parse a io-written buffer correctly" do
+    write = []
     for i in 1...10 do
-      @chain.write_message(@w, "x#{i}")
+      write << "x#{i}"
     end
+    chain_write(*write)
+
     @chain.from(@r.readpartial(4096)).should == %w{x1 x2 x3 x4 x5 x6 x7 x8 x9}
   end
 
   it "should read messages greater than MTU correctly" do
-    @chain.write_message(@w, message = ("M" * (Arpie::MTU + 10)))
-    @chain.read_message(@r).should == message
+    chain_write(message = ("M" * (Arpie::MTU + 10)))
+    chain_read.should == message
   end
 
   it "should not fail on interleaved io streams" do
     r2, w2 = IO.pipe
-    @chain.write_message(@w, "1" * 2048)
+    chain_write("1" * 2048)
     @chain.write_message(w2, "2" * 2048)
-    @chain.read_message(@r).should == "1" * 2048
+    w2.close
+    chain_read.should == "1" * 2048
     @chain.read_message(r2).should == "2" * 2048
   end
 end
@@ -64,11 +74,13 @@ describe "ObjectProtocolChain", :shared => true do
   # Now, lets try some variations.
 
   it "should read written objects correctly" do
-    @chain.write_message(@w, a = [1, 2.4, false, true, nil, "string"])
-    @chain.write_message(@w, b = {1 => "hi", 2 => "test", 3 => "bloh"})
+    chain_write(
+      a = [1, 2.4, false, true, nil, "string"],
+      b = {1 => "hi", 2 => "test", 3 => "bloh"}
+    )
 
-    @chain.read_message(@r).should == a
-    @chain.read_message(@r).should == b
+    chain_read.should == a
+    chain_read.should == b
   end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,20 @@ describe "IO Mockup", :shared => true do
   before do
     @r, @w = IO.pipe
   end
+
+  def chain_write *m
+    m.each {|mm|
+      @chain.write_message(@w, mm)
+    }
+    @w.close
+  end
+
+  def chain_read
+    @chain.read_message(@r)
+  end
+
+  def chain_r_w *m
+    chain_write *m
+    chain_read
+  end
 end
