51cd63dbbd14e58861957c46260a7e1d2d2ac999
Bernhard Stoeckner <elven@swordcoast.net>
2011-07-22 16:27:46 +0200

    Arpie::EventMachine::ArpieProtocol: EM::Connection protocol to encode via arpie protocol chains

---
+++ lib/arpie/em.rb
@@ -0,0 +1,51 @@
+require 'eventmachine'
+
+module Arpie
+  module EventMachine
+    # A EventMachine protocol implementing a simple protocol
+    # chain handler. To use, simply include it in your EM
+    # Connection moduleas you would any other EM protocol.
+    #
+    # The module expects a list of protocols given along with
+    # the module initializer:
+    #   EM::start_server host, port, ArpieProtocol,
+    #     Arpie::MarshalProtocol.new, Arpie::SizedProtocol.new
+    #
+    # To receive messages, override <tt>receive(message)</tt>, which will
+    # be called once for each message decoded with the given
+    # protocols.
+    #
+    # To send messages back over the same connection, simply call
+    # <tt>send(message)</tt>.
+    #
+    # Note that this module isn't included by requiring just 'arpie'.
+    # You will need to require 'arpie/em'.
+    module ArpieProtocol
+      attr_reader :chain
+
+      def initialize *protocols
+        @chain = Arpie::ProtocolChain.new(*protocols)
+      end
+
+      # Receive a message. Override this in your implemention.
+      def receive message
+      end
+
+      def receive_data data
+        begin
+          for msg in @chain.from(data)
+            receive msg
+          end
+        rescue Arpie::EIncomplete
+          nil
+        end
+      end
+
+      # Send a message, encoding it with the given
+      # protocols.
+      def send message
+        send_data(@chain.to(message))
+      end
+    end
+  end # module EventMachine
+end # module Arpie
---
+++ spec/em_spec.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+require 'arpie/em'
+
+describe 'Arpie::EventMachine::ArpieProtocol' do
+  specify do
+    $m = nil
+
+    EM::run {
+      EM::start_server "127.0.0.1", 19400, Arpie::EventMachine::ArpieProtocol,
+          Arpie::SeparatorProtocol.new do |proto|
+        def proto.receive(message)
+          $m = message
+          EventMachine::stop_event_loop
+        end
+      end
+
+      EM::connect "127.0.0.1", 19400, Arpie::EventMachine::ArpieProtocol,
+          Arpie::SeparatorProtocol.new do |proto|
+
+        proto.send("hi")
+      end
+    }
+
+    $m.should == "hi"
+  end
+end
